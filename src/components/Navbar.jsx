11866f18f5594c88b7edcf821ef8522bfa41b967
sahilchaubey03 <115353207+Sahil-Chaubey@users.noreply.github.com>
2023-10-10 20:01:48 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,11 +34,6 @@ export default function Navbar() {
       </HStack>
     </Flex>
 
-    // <Flex bg={"gray.200"} justify={"space-between"} wrap={"wrap"} gap={"2"}>
-    //     <Box w="150px" h="50px" bg="red">1</Box>
-    //     <Box w="150px" h="50px" bg="blue">2</Box>
-    //     <Box w="150px" h="50px" bg="green">3</Box>
-    //     <Box w="150px" h="50px" bg="yellow">4</Box>
-    // </Flex>
+    
   );
 }
